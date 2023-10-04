ef27307ad5a730d209c43aa38484708a2ce20395
AveenBejan <aveen_eng>
2023-10-04 16:53:31 +0300

    commit today

--- start_server.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-FOR /F "delims=: tokens=2" %%a in ('ipconfig ^| find "IPv4"') do set _IPAddress=%%a
-cd /d %~dp0 & "E:\projects\dentistsite\virtual\Scripts\activate" & python manage.py runserver %_IPAddress%:8080
-cmd /k
-
-
-
