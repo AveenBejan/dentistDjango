055d18e6e684c2bd66da9da8032ac15db2ef7b5d
AveenBejan <aveen_eng>
2023-11-22 13:55:40 +0300

    commit today

--- dentist/settings.py
+++ dentist/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dentist.wsgi.application'
 
 
-
 DATABASES = {
 
     'default': dj_database_url.config(
@@ -85,8 +84,6 @@ DATABASES = {
 
     )
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
