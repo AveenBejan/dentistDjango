e94f7b05bf4cb202a849386a006b80870c6871ee
AveenBejan <aveen_eng>
2024-10-15 20:41:59 +0300

    comment today

--- website/forms.py
+++ website/forms.py
@@ -352,6 +352,7 @@ class DrugForm(forms.ModelForm):
         ('Capsule', 'Capsule'),
         ('Drop', 'Drop'),
         ('Injection', 'Injection'),
+        ('Jel', 'Jel'),
     ]
     DISPENSE_CHOICES = [
         ('', 'Select Dispense'),
